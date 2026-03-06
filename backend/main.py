462ba3dcaa9571efd563e31932dc2fe9312feeb9
Michael Florip <michaelflorip@berkeley.edu>
2026-03-05 16:42:02 -0800

    feat: allow_origin to include vercel

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,11 @@ app = FastAPI(title="DSPA Bot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "https://michael-dspa-frontend.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
